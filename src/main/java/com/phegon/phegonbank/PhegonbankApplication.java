56d634d46e90cb852689e5697b23276e733253d2
phegondev <phegondev@gmail.com>
2025-09-28 09:08:10 +0100

    backnd implamenrtation v1 complete

--- src/main/java/com/phegon/phegonbank/PhegonbankApplication.java
+++ src/main/java/com/phegon/phegonbank/PhegonbankApplication.java
@@ -1,13 +1,39 @@
 package com.phegon.phegonbank;
 
+import com.phegon.phegonbank.auth_users.entity.User;
+import com.phegon.phegonbank.enums.NotificationType;
+import com.phegon.phegonbank.notification.dtos.NotificationDTO;
+import com.phegon.phegonbank.notification.services.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@RequiredArgsConstructor
 public class PhegonbankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhegonbankApplication.class, args);
-	}
+//    private final NotificationService notificationService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhegonbankApplication.class, args);
+    }
+
+//    @Bean
+//    CommandLineRunner runner(){
+//        return args -> {
+//            NotificationDTO notificationDTO = NotificationDTO.builder()
+//                    .recipient("therecepientemail@gmail.com")
+//                    .subject("HEllo testing email")
+//                    .body("Hey, this is a test eamil 😁")
+//                    .type(NotificationType.EMAIL)
+//                    .build();
+//
+//            notificationService.sendEmail(notificationDTO, new User());
+//        };
+//    }
 
 }
---
+++ src/main/java/com/phegon/phegonbank/account/dtos/AccountDTO.java
@@ -0,0 +1,56 @@
+package com.phegon.phegonbank.account.dtos;
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.phegon.phegonbank.auth_users.dtos.UserDTO;
+import com.phegon.phegonbank.auth_users.entity.User;
+import com.phegon.phegonbank.enums.AccountStatus;
+import com.phegon.phegonbank.enums.AccountType;
+import com.phegon.phegonbank.enums.Currency;
+import com.phegon.phegonbank.transaction.dtos.TransactionDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDTO {
+
+    private Long id;
+
+    private String accountNumber;
+
+    private BigDecimal balance;
+
+    private AccountType accountType;
+
+    @JsonBackReference// this will not be added to the account dto. It will be ignored because it is a back refrence
+    private UserDTO user;
+
+    private Currency currency;
+
+    private AccountStatus status;
+
+    @JsonManagedReference// if helps avoid recursion loop by ignoring the AccountDTO withing the TransactionDTO
+     private List<TransactionDTO> transactions;
+
+    private LocalDateTime closedAt;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
